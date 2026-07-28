120386553b14ba31871bcd6dd7d523a583e3c91b
mateuskauadev <mateuskauaferreira9@gmail.com>
2026-07-27 22:57:26 -0300

    adicionando exercicios

---
+++ exercicios-especiais/Area_circulo.py
@@ -0,0 +1,9 @@
+print("--------------------------------------")
+print("   calculador da area do circulo")
+print("--------------------------------------")
+print("Iremos calcular a area do circulo")
+raio = float(input("Digite o Raio:"))
+pi = 3.14
+raio_ao_quadrado = raio ** 2
+area = 3.14 * raio_ao_quadrado
+print("A área do circulo sera de", area, "metros quadrados")
---
+++ exercicios-especiais/Entrevista.py
@@ -0,0 +1,10 @@
+print("-----------------------------------")
+print("           entrevista")
+print("-----------------------------------")
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}! Iremos fazer uma entrevista com você.")
+print("Seu nome tem", len(nome), "letras.")
+#calculo da idade
+idade = int(input("Digite a sua idade: "))
+idade_futura = idade + 5
+print("Daqui 5 anos você tera", idade_futura, "anos.")
